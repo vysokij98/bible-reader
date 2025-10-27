83b48ad71424a62e134f305bdd07dd4904e12ff2
vysokij98 <vysokij98@gmail.com>
2025-10-27 17:28:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,4 +34,6 @@ def alice():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
